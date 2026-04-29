2daefe4165f030c5ed32b2b53d31256f6671fbba
HyshaDadwal <paridadwal21@gmail.com>
2026-04-29 12:05:02 +0530

    frontend ready

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.https://expense-backend-mp26.onrender.com
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 API.interceptors.request.use((req) => {
